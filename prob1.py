a95bce62b4f061ea088ec61aaaac308d1448220b
DavidGildour <davidgildour@mail.com>
2019-05-29 16:01:06 +0200

    changed approach, used api, first task works fine

--- prob1.py
+++ prob1.py
@@ -1,14 +1,22 @@
 import requests
+import json
 
-from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
+cams_raw = requests.get('http://api.deckchair.com/v1/cameras').content
+cams_json = json.loads(cams_raw)['data']
 
-with webdriver.Firefox() as driver:
-    driver.get('https://camera.deckchair.com/st-regis-le-morne-mauritius')
-    assert 'Global HD Live Webcams' in driver.title
+target_cam = ''
+for cam in cams_json:
+    if 'Mauritius' in cam['title']:
+        target_cam = cam
+        break
+print(target_cam['title'])
 
-    elem = driver.find_element_by_class_name('action-button')
-    url = elem.get_attribute('href')
+id = target_cam['_id']
+cam_imgs_raw = requests.get(f'http://api.deckchair.com/v1/camera/{id}/images').content
+cam_imgs_json = json.loads(cam_imgs_raw)['data']
+
+img_id = cam_imgs_json[0]['_id']
+url = f'http://api.deckchair.com/v1/viewer/image/{img_id}'
 
 with open('test.jpg', 'wb') as f:
     r = requests.get(url)
